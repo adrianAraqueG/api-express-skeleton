51b53fc2e4b94a8402fc80470255e5eb07d9c059
adrianaraqueg <adrian.kanox.2003@gmail.com>
2024-05-09 09:26:07 -0500

    feat: :sparkles: Se agregó un manejador de excepciones extra para evitar cualquier detención de la aplicación

--- src/main.js
+++ src/main.js
@@ -5,5 +5,8 @@
  */
 import App from "./app.js";
 
+process.on('uncaughtException', stream => console.log(stream));
+process.on('unhandledRejection', stream => console.log(stream));
+
 const service = new App();
 service.listen();
